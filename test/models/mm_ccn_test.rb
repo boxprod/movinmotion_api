fff66d8519146c82a43c929e253926f7325c2289
Victor Holl <v_h@me.com>
2022-11-25 17:58:28 +0100

    test inheritance tentative

--- test/models/mm_ccn_test.rb
+++ test/models/mm_ccn_test.rb
@@ -1,4 +1,13 @@
 require "test_helper"
+require_relative 'mm_test'
 
-class MmCcnTest < ActiveSupport::TestCase
+class MmCcnTest < MmTest
+  def setup
+    @class = MmCcn
+  end
+
+  test 'it responds to #positions' do
+    positions = MmCcn.all.sample.positions
+    assert positions.is_a? Array
+  end
 end
--- test/models/mm_test.rb
+++ test/models/mm_test.rb
@@ -1,39 +1,48 @@
 require "test_helper"
 
-class MmProjectTest < ActiveSupport::TestCase
+class MmTest < ActiveSupport::TestCase
   MovinmotionApi.configure do |mm|
     mm.use_classes = true
   end
 
+  def setup
+    @class = Mm
+  end
+
+
+  test 'class name is defined' do
+    assert @class unless is_a? MmTest
+  end
+
   test 'it responds to #all' do
-    all = MmProject.all
+    all = @class.all
     assert all
   end
 
   test 'it responds to #first' do
-    first = MmProject.first
+    first = @class.first
     assert first
   end
 
   test 'it responds to #find' do
-    id = MmProject.all.sample.id
-    assert MmProject.find(id)
+    id = @class.all.sample.id
+    assert @class.find(id)
   end
 
   test 'it responds to #count' do
-    count = MmProject.count
+    count = @class.count
     assert count
     assert count.instance_of? Integer
   end
 
   test 'it responds to #where' do
-    where = MmProject.where(idcc_number: '2642')
+    where = @class.where(idcc_numbers: '2642')
     assert where
     assert_not where.empty?
   end
 
   test 'it raises an error if wrong type in #where' do
-    MmProject.where(idcc_number: 2642)
+    @class.where(idcc_numbers: 2642)
   rescue StandardError => e
     assert e.message.include?('class')
   end
