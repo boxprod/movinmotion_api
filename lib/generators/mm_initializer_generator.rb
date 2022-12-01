0c136c08cf6c9f9c4de87b79596a3a55e782a652
Victor Holl <v_h@me.com>
2022-12-01 14:54:02 +0100

    update initializer

--- lib/generators/mm_initializer_generator.rb
+++ lib/generators/mm_initializer_generator.rb
@@ -2,10 +2,11 @@ class MmInitializerGenerator < Rails::Generators::Base
   desc "This generator creates an initializer file at config/initializers"
   def create_initializer_file
     create_file "config/initializers/movinmotion_api.rb",
-    "MovinmotionApi.configure do |mm|
-      mm.cookie = ENV['MOVINMOTION_COOKIE']
-      mm.use_classes = true
-    end
-    "
+"MovinmotionApi.configure do |mm|
+  mm.cookie = ENV['MOVINMOTION_COOKIE']
+  mm.use_classes = true
+  mm.write_access = false
+end
+"
   end
 end
